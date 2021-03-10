6dc3ff8e7b88fc713a45b74ebfdc584ef2dc056e
k1te <kmiller343@gmail.com>
2021-03-10 14:02:03 -0600

    imported useHistory to push individual game page

--- client/src/features/games/gamelist/GameList.js
+++ client/src/features/games/gamelist/GameList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUserGames, selectUserGamesLoading } from "../gamesSlice";
+import { useHistory } from "react-router-dom";
 
 const GameList = () => {
   const gamesLoading = useSelector(selectUserGamesLoading);
   const userGames = useSelector(selectUserGames);
 
   console.log(userGames);
+  let history = useHistory();
 
   let userGameList = <p>No games found.</p>;
 
@@ -27,8 +29,15 @@ const GameList = () => {
     userGameList = userGames.map((game) => (
       <div className="card" key={game._id}>
         <div className="card-body">
-          <h3 className="card-title">{formatDate(game.date)}</h3>
-          <h5 className="card-subtitle">Winnings: ${game.score}</h5>
+          <a
+            onClick={() => {
+              localStorage.setItem("game", game._id);
+              history.push(`/${game._id}`);
+            }}
+          >
+            <h3 className="card-title">{formatDate(game.date)}</h3>
+            <h5 className="card-subtitle">Winnings: ${game.score}</h5>
+          </a>
         </div>
       </div>
     ));

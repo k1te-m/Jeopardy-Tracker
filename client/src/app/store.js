4767ec3f69644a8328362077d783aaa36aacbd3c
k1te <kmiller343@gmail.com>
2021-03-11 12:08:20 -0600

    rm gameModalSlice/Reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import gamesReducer from "../features/games/gamesSlice";
-import gameModalReducer from "../features/games/gameModalSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     games: gamesReducer,
-    gameModal: gameModalReducer,
   },
 });

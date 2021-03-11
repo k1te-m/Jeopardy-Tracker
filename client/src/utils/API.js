4ee79aaeab88b04def14567976e3baca252908c8
k1te <kmiller343@gmail.com>
2021-03-11 14:09:09 -0600

    created updateScore function

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,4 +10,7 @@ export default {
   getGameById: async (id) => {
     return await axios.get("/api/game/set/" + id);
   },
+  updateScore: async (id, data) => {
+    return await axios.put("/api/game/save/" + id, data);
+  },
 };

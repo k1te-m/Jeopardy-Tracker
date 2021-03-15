c548c820e0a0baab577b7718ba3b9f6d18e527c3
k1te <kmiller343@gmail.com>
2021-03-15 13:16:30 -0500

    added API routes for getting user/games 4 profile

--- routes/api/user.js
+++ routes/api/user.js
@@ -5,6 +5,11 @@ const userController = require("../../controllers/userController");
 // @route   GET api/user/:id
 // @desc    Get all of user games
 // @access  Private
-router.get("/:id", userController.getUser);
+router.get("/:username", auth, userController.getUser);
+
+// @route   GET api/user/:id
+// @desc    Get all of user games
+// @access  Private
+router.get("/games/:username", auth, userController.getProfileGames);
 
 module.exports = router;

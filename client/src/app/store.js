5e498a778c4e20f132870f25fd6d2fb11614a6f4
k1te <kmiller343@gmail.com>
2021-03-15 13:14:00 -0500

    imported profileReducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import gamesReducer from "../features/games/gamesSlice";
 import alertReducer from "../features/alert/alertSlice";
+import profileReducer from "../features/profile/profileSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     games: gamesReducer,
+    profile: profileReducer,
     alert: alertReducer,
   },
 });

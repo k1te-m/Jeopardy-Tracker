7a46e6c4ce19988213ba77deb5cc7178a6ba0cc2
k1te <kmiller343@gmail.com>
2021-03-15 13:15:55 -0500

    getUser/ getPorfileGames functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,19 @@
 const User = require("../models/User");
+const Game = require("../models/Game");
 
 module.exports = {
   getUser: (req, res) => {
-    const id = req.params.id;
-    User.find({ _id: id })
+    const username = req.params.username;
+    User.findOne({ username: username })
       .select("-password")
       .then((user) => res.json(user))
+      .catch((error) => res.status(422).json(error));
+  },
+  getProfileGames: (req, res) => {
+    const username = req.params.username;
+    Game.find({ username: username })
+      .sort({ date: -1 })
+      .then((games) => res.json(games))
       .catch((error) => res.status(422).json(error));
   },
 };

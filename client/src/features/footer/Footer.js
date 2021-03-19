70b7b73aa30b3d467d5e1e0e7400f906034ba25c
k1te <kmiller343@gmail.com>
2021-03-19 02:02:04 -0500

    updated footer structure

--- client/src/features/footer/Footer.js
+++ client/src/features/footer/Footer.js
@@ -12,46 +12,63 @@ const Footer = () => {
     <div className="footer">
       <div className="container">
         <div className="row">
-          <p className="disclaimer">
-            Disclaimer: This website is in no way affiliated with Jeopardy!, CBS
-            Media Ventures, or Jeopardy Productions, Inc. It is only meant as a
-            tool to enhance your viewing experience.
-          </p>
+          <div className="col-4">
+            <div className="row">
+              <Link to="/">Home</Link>
+            </div>
+            {auth.user && (
+              <div className="row">
+                <button
+                  onClick={() => {
+                    history.push(`/profile/${auth.user.username}`);
+                  }}
+                >
+                  Profile
+                </button>
+              </div>
+            )}
+            <div className="row">
+              <a
+                href="https://standuptocancer.org/research/research-portfolio/dream-teams/transforming-pancreatic-cancer-dream-team/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                SU2C - Pancreatic Cancer Research
+              </a>
+            </div>
+            <div className="row">
+              <a
+                href="https://www.jeopardy.com/contestant-zone"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Current Jeopardy Contestants
+              </a>
+            </div>
+            <div className="row">
+              <a
+                href="https://github.com/k1te-m/Jeopardy-Tracker"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repository
+              </a>
+            </div>
+          </div>
+          <div className="col-8">
+            <p className="disclaimer">
+              Disclaimer: This website is in no way affiliated with Jeopardy!,
+              CBS Media Ventures, or Jeopardy Productions, Inc. It is only meant
+              as a tool to enhance your viewing experience.
+            </p>
+            <p className="donate">
+              Please consider donating to the SU2C Pancreatic Cancer Research
+              Fund or your charity of choice.
+            </p>
+          </div>
         </div>
         <div className="row">
-          <p className="donate">
-            Please consider donating to the SU2C Pancreatic Cancer Research Fund
-            or your charity of choice.
-          </p>
-        </div>
-        <div className="row row-cols-3">
-          <Link to="/">Home</Link>
-          {auth.user && (
-            <button
-              onClick={() => {
-                history.push(`/profile/${auth.user.username}`);
-              }}
-            >
-              Profile
-            </button>
-          )}
-          <a
-            href="https://standuptocancer.org/research/research-portfolio/dream-teams/transforming-pancreatic-cancer-dream-team/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            SU2C - Pancreatic Cancer Research
-          </a>
-          <a
-            href="https://www.jeopardy.com/contestant-zone"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Current Jeopardy Contestants
-          </a>
-        </div>
-        <div className="row mt-3">
-          <p className="copyright">© 2021 True Daily Double</p>
+          <span className="copyright">True Daily Double © 2021</span>
         </div>
       </div>
     </div>
